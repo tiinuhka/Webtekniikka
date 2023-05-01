2a94260940c924de4ca68290f9fbb1618f1e6217
tiinuhka <101732102+tiinuhka@users.noreply.github.com>
2023-05-01 20:03:34 +0300

    added comments and css

--- js/script.js
+++ js/script.js
@@ -9,24 +9,30 @@ document.getElementById("submit-name").onclick=function(){
     //get the note and save it in variable "note"
     let note = document.getElementById("note").value;
 
-    
+    //get the date and time of the visit
     let date = new Date();
 
 
     //check if the checkbox is checked
     let ischecked = document.getElementById("important").checked;
-    //conditional syntax
+    
+    //set the divclass value unchecked by default
     let divclass = "unchecked";
     if (ischecked)
     {
+    
+        //set the divclass checked if checkbox is checked
         divclass = "checked";
     }
 
     //add the new visitor to the list
+    //use backslash as an escape character to add quotes inside the string 
     let text = "<div class=\"" +  divclass + "\">";
     text += name + "." + " " + note + " " + date + "<br>";
     text += "</div>";
     document.getElementById("visitor-name").innerHTML += text;
-return false;
+
+    //the info is not being sent forward from this form
+    return false;
 
 }
